bcae6472eb1523106f373e2281e38d5ccd6a0d38
Yunus BAYDAR <95569715+yunusbaydarr@users.noreply.github.com>
2026-03-01 14:28:29 +0300

    Resolve BeanPostProcessor eager initialization warning by using Binder

--- src/main/java/com/nplus1_hunter/Nplus1_Hunter/autoconfigure/NPlusOneAutoConfiguration.java
+++ src/main/java/com/nplus1_hunter/Nplus1_Hunter/autoconfigure/NPlusOneAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.nplus1_hunter.Nplus1_Hunter.autoconfigure;
 
+import com.nplus1_hunter.Nplus1_Hunter.datasource.DataSourceBeanPostProcessor;
 import com.nplus1_hunter.Nplus1_Hunter.web.NPlusOneFilter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @EnableConfigurationProperties(NPlusOneProperties.class)
@@ -14,19 +16,17 @@ import org.springframework.context.annotation.Configuration;
 public class NPlusOneAutoConfiguration {
 
     @Bean
-    public static DataSourceBeanPostProcessor dataSourceBeanPostProcessor(NPlusOneProperties properties) {
-        return new DataSourceBeanPostProcessor(properties);
+    public static DataSourceBeanPostProcessor dataSourceBeanPostProcessor(Environment environment) {
+        return new DataSourceBeanPostProcessor(environment);
     }
 
     @Bean
     @ConditionalOnWebApplication
     public FilterRegistrationBean<NPlusOneFilter> nPlusOneFilter() {
         FilterRegistrationBean<NPlusOneFilter> registrationBean = new FilterRegistrationBean<>();
-
         registrationBean.setFilter(new NPlusOneFilter());
         registrationBean.addUrlPatterns("/*");
         registrationBean.setOrder(Integer.MIN_VALUE);
-
         return registrationBean;
     }
 }
--- src/main/java/com/nplus1_hunter/Nplus1_Hunter/datasource/DataSourceBeanPostProcessor.java
+++ src/main/java/com/nplus1_hunter/Nplus1_Hunter/datasource/DataSourceBeanPostProcessor.java
@@ -1,17 +1,22 @@
-package com.nplus1_hunter.Nplus1_Hunter.autoconfigure;
+package com.nplus1_hunter.Nplus1_Hunter.datasource;
 
-import com.nplus1_hunter.Nplus1_Hunter.datasource.QueryCaptureListener;
+import com.nplus1_hunter.Nplus1_Hunter.autoconfigure.NPlusOneProperties;
 import net.ttddyy.dsproxy.support.ProxyDataSourceBuilder;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.core.env.Environment;
+
 import javax.sql.DataSource;
 
 public class DataSourceBeanPostProcessor implements BeanPostProcessor {
 
     private final NPlusOneProperties properties;
 
-    public DataSourceBeanPostProcessor(NPlusOneProperties properties) {
-        this.properties = properties;
+    public DataSourceBeanPostProcessor(Environment environment) {
+        this.properties = Binder.get(environment)
+                .bind("nplus1", NPlusOneProperties.class)
+                .orElseGet(NPlusOneProperties::new);
     }
 
     @Override
